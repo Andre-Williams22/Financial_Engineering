86c8fca68917470c9e6295a90a4ab97e81b99925
Andre Williams <awjrs22@gmail.com>
2021-03-11 22:04:09 -0500

    update rl startegy

--- rl/linear_rl_trader.py
+++ rl/linear_rl_trader.py
@@ -11,6 +11,21 @@ import pickle
 
 from sklearn.preprocessing import StandardScaler
 
+import requests
+import json  as json
+import alpaca_trade_api as tradeapi 
+import time 
+
+
+endpoint = "https://data.alpaca.markets/v1"
+headers = json.loads(open("account.json",'r').read())
+api = tradeapi.REST(headers["APCA-API-KEY-ID"], headers["APCA-API-SECRET-KEY"], base_url='https://paper-api.alpaca.markets')
+tickers = "SBUX,MSI,FB,XM,SAP,LNSR,FSLR,NIO,LMND,FTCH,PSTH,DASH,SBE,PFE,CAKE,SPCE,RVLV,FTCH,AMZN,INTC,PLTR,ELY,CCIV,MSFT,AAPL,GOOG,CSCO,CMCSA,ADBE,NVDA,NFLX,PYPL,AMGN,AVGO,TXN,CHTR,QCOM,GILD,FISV,BKNG,INTU,ADP,CME,TMUS,MU"
+max_pos = 1000 #max position size for each ticker
+stoch_signal = {}
+
+account = api.get_account()
+
 
 # Let's use AAPL (Apple), MSI (Motorola), SBUX (Starbucks)
 def get_data():
@@ -22,9 +37,45 @@ def get_data():
   df = pd.read_csv('../tf2.0/aapl_msi_sbux.csv')
   return df.values
 
+def hist_data(symbols, timeframe="15Min", limit=200, start="", end="", after="", until=""):
+    '''Returns the historical bar data for a group of stocks '''
+    df_data = {}
+    # Get Requests for Bar Data
+    bar_url = endpoint + "/bars/{}".format(timeframe)
+
+    params = {
+        "symbols": symbols,
+        "limit": limit,
+        "start": start,
+        "end": end,
+        "after": after,
+        "until": until
+    }
 
 
+    r = requests.get(bar_url, headers=headers, params=params)
 
+    json_dump = r.json()
+    # loop through stock data 
+    for symbol in json_dump:
+        # print("symbol = ",symbol)
+        # print(json_dump[symbol])
+        # convert json into pandas dataframe 
+        temp = pd.DataFrame(json_dump[symbol])
+        temp = temp['c']
+        # temp.rename({"t":"time", "c":"close"}, axis=1, inplace=True)
+        # temp.rename({"t":"time", "o": "open", 'h':'high', 'l':'low', "c":"close", 'v':'volume'}, axis=1, inplace=True)
+        # temp['time'] = pd.to_datetime(temp['time'], unit='s')
+        # temp.set_index("time", inplace=True)
+        # eastern = pytz.timezone('US/Eastern')
+        # temp.index = temp.index.tz_localize(pytz.utc).tz_convert(eastern) 
+        
+        # append data to df data 
+        df_data[symbol] = temp
+        
+    df_data = pd.DataFrame(df_data)
+        
+    return df_data.values
 
 def get_scaler(env):
   # return scikit-learn scaler object to scale the states
@@ -46,13 +97,14 @@ def get_scaler(env):
 
 
 def maybe_make_dir(directory):
+  '''Stores trained model in a file '''
   if not os.path.exists(directory):
     os.makedirs(directory)
 
 
 
 class LinearModel:
-  """ A linear regression model """
+  """ A linear regression model with SGD """
   def __init__(self, input_dim, n_action):
     self.W = np.random.randn(input_dim, n_action) / np.sqrt(input_dim)
     self.b = np.zeros(n_action)
@@ -61,6 +113,7 @@ class LinearModel:
     self.vW = 0
     self.vb = 0
 
+    # Will populate at each step of gradient descent
     self.losses = []
 
   def predict(self, X):
@@ -70,7 +123,7 @@ class LinearModel:
 
   def sgd(self, X, Y, learning_rate=0.01, momentum=0.9):
     # make sure X is N x D
-    assert(len(X.shape) == 2)
+    assert(len(X.shape) == 2) # make sure vector is 2-dimensional
 
     # the loss values are 2-D
     # normally we would divide by N only
@@ -93,10 +146,12 @@ class LinearModel:
     self.W += self.vW
     self.b += self.vb
 
+    # calc mse to evaluate performance
     mse = np.mean((Yhat - Y)**2)
     self.losses.append(mse)
 
   def load_weights(self, filepath):
+    '''Loads pretrained weights '''
     npz = np.load(filepath)
     self.W = npz['W']
     self.b = npz['b']
@@ -124,7 +179,8 @@ class MultiStockEnv:
     - 1 = hold
     - 2 = buy
   """
-  def __init__(self, data, initial_investment=20000):
+  
+  def __init__(self, data, initial_investment=account.equity): # add alpaca api
     # data
     self.stock_price_history = data
     self.n_step, self.n_stock = self.stock_price_history.shape
@@ -233,6 +289,8 @@ class MultiStockEnv:
       # NOTE: to simplify the problem, when we sell, we will sell ALL shares of that stock
       for i in sell_index:
         self.cash_in_hand += self.stock_price[i] * self.stock_owned[i]
+        filled_qty = api.get_position(i).qty
+        api.submit_order(symbol=i, qty=int(filled_qty), side="sell", type="trailing_stop", time_in_force="day", trail_percent = "1.5")
         self.stock_owned[i] = 0
     if buy_index:
       # NOTE: when buying, we will loop through each stock we want to buy,
@@ -242,6 +300,7 @@ class MultiStockEnv:
         for i in buy_index:
           if self.cash_in_hand > self.stock_price[i]:
             self.stock_owned[i] += 1 # buy one share
+            api.submit_order(symbol=i, qty=1, side="buy",type="market", time_in_force="ioc")
             self.cash_in_hand -= self.stock_price[i]
           else:
             can_buy = False
@@ -310,19 +369,23 @@ def play_one_episode(agent, env, is_train):
 
 
 if __name__ == '__main__':
+  
+  account = api.get_account()
 
   # config
   models_folder = 'linear_rl_trader_models'
   rewards_folder = 'linear_rl_trader_rewards'
   num_episodes = 2000
   batch_size = 32
-  initial_investment = 20000
+  initial_investment = float(account.equity) - float(account.last_equity)
 
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-m', '--mode', type=str, required=True,
-                      help='either "train" or "test"')
-  args = parser.parse_args()
+  # parser = argparse.ArgumentParser()
+  # parser.add_argument('-m', '--mode', type=str, required=True,
+  #                     help='either "train" or "test"')
+  # args = parser.parse_args()
+  
+  mode = 'test'
 
   maybe_make_dir(models_folder)
   maybe_make_dir(rewards_folder)
@@ -333,7 +396,10 @@ if __name__ == '__main__':
   n_train = n_timesteps // 2
 
   train_data = data[:n_train]
-  test_data = data[n_train:]
+  
+  tickers = "AAPL,PLTR,LMND"
+  
+  test_data = get_data(tickers)#data[n_train:]
 
   env = MultiStockEnv(train_data, initial_investment)
   state_size = env.state_dim
@@ -344,7 +410,8 @@ if __name__ == '__main__':
   # store the final value of the portfolio (end of episode)
   portfolio_value = []
 
-  if args.mode == 'test':
+  #if args.mode == 'test':
+  if mode == 'test':
     # then load the previous scaler
     with open(f'{models_folder}/scaler.pkl', 'rb') as f:
       scaler = pickle.load(f)
@@ -362,13 +429,13 @@ if __name__ == '__main__':
   # play the game num_episodes times
   for e in range(num_episodes):
     t0 = datetime.now()
-    val = play_one_episode(agent, env, args.mode)
+    val = play_one_episode(agent, env, mode)
     dt = datetime.now() - t0
     print(f"episode: {e + 1}/{num_episodes}, episode end value: {val:.2f}, duration: {dt}")
     portfolio_value.append(val) # append episode end portfolio value
 
   # save the weights when we are done
-  if args.mode == 'train':
+  if mode == 'train':
     # save the DQN
     agent.save(f'{models_folder}/linear.npz')
 
